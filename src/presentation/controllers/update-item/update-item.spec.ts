7696f4dc1563b0d8e5d082ea9a6e6ac7d396a2db
João Vitor Marques <jvsilvam@outlook.com>
2024-01-28 19:47:55 -0300

    test: ensure UpdateItemController returns 400 if invalid data is provided

---
+++ src/presentation/controllers/update-item/update-item.spec.ts
@@ -0,0 +1,50 @@
+import {
+  ItemModel,
+  UpdateItem,
+  MissingParamError,
+  UpdateItemController,
+} from "./update-item-protocols"
+
+interface SutTypes {
+  sut: UpdateItemController
+  updateItemStub: UpdateItem
+}
+
+const makeUpdateItem = (): UpdateItem => {
+  class UpdateItemStub implements UpdateItem {
+    update(item: ItemModel): Promise<ItemModel> {
+      return new Promise((resolve) => resolve(item))
+    }
+  }
+
+  return new UpdateItemStub()
+}
+
+const makeSut = (): SutTypes => {
+  const updateItemStub = makeUpdateItem()
+  const sut = new UpdateItemController(updateItemStub)
+
+  return {
+    sut,
+    updateItemStub,
+  }
+}
+
+describe("UpdateItem Controller", () => {
+  test("Should return 400 if invalid data is provided", async () => {
+    const { sut } = makeSut()
+
+    const httpRequest = {
+      body: {
+        id: "valid_id",
+        name: "valid_name",
+        amount: 2,
+      },
+    }
+
+    const httpResponse = await sut.handle(httpRequest)
+
+    expect(httpResponse.statusCode).toBe(400)
+    expect(httpResponse.body).toEqual(new MissingParamError("categoryId"))
+  })
+})
