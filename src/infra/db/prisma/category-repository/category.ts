4ff9f1bfd7cf13c1e83f3d8278427c2d2d313bcf
João Vitor Marques <jvsilvam@outlook.com>
2024-02-05 20:23:40 -0300

    feat: ensure PrismaCategoryRepository add method returns a category on success

---
+++ src/infra/db/prisma/category-repository/category.ts
@@ -0,0 +1,17 @@
+import db from "../helpers/prisma-helper"
+import { CategoryModel } from "@/domain/models/category"
+import { CategoryRepository } from "@/data/protocols/category-repository"
+
+export class PrismaCategoryRepository implements CategoryRepository {
+  async add(name: string): Promise<CategoryModel> {
+    return await db.category.create({
+      data: {
+        name,
+      },
+    })
+  }
+
+  async list(): Promise<CategoryModel[]> {
+    return await db.category.findMany()
+  }
+}
