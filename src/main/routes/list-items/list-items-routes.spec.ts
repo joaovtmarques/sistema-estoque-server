b18c06d531bef6ccf4b4d16b39cc26a0ebcd405d
João Vitor Marques <jvsilvam@outlook.com>
2024-02-23 20:47:23 -0300

    test: ensure ListItem route list all items on success

---
+++ src/main/routes/list-items/list-items-routes.spec.ts
@@ -0,0 +1,40 @@
+import request from "supertest"
+import app from "@/main/config/app"
+import { db } from "@/infra/db/prisma/helpers/prisma-helper"
+
+describe("List items route", () => {
+  afterEach(async () => {
+    await db.category.deleteMany({})
+    await db.item.deleteMany({})
+  })
+
+  test("Should list all items on success", async () => {
+    const category = await request(app)
+      .post("/api/categories")
+      .send({ name: "valid_category" })
+
+    await request(app).post("/api/items").send({
+      name: "valid_name",
+      categoryId: category.body.id,
+      amount: 1,
+    })
+
+    const { body } = await request(app).get("/api/items").expect(200)
+
+    expect(body).toBeTruthy()
+    expect(body).toEqual([
+      {
+        id: expect.any(String),
+        name: "valid_name",
+        categoryId: category.body.id,
+        category: {
+          id: expect.any(String),
+          name: "valid_category",
+        },
+        model: null,
+        serialNumber: null,
+        amount: 1,
+      },
+    ])
+  })
+})
