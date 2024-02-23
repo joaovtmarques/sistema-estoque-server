ec04b27c5ecfae4196c92d9fc2c61af7c9ebb4b2
João Vitor Marques <jvsilvam@outlook.com>
2024-02-23 20:28:35 -0300

    test: ensure AddItem route returns an item on success

---
+++ src/main/routes/add-item/add-item-routes.spec.ts
@@ -0,0 +1,35 @@
+import request from "supertest"
+import app from "@/main/config/app"
+import { db } from "@/infra/db/prisma/helpers/prisma-helper"
+
+describe("Add item route", () => {
+  afterEach(async () => {
+    await db.category.deleteMany({})
+    await db.item.deleteMany({})
+  })
+
+  test("Should return an item on success", async () => {
+    const category = await request(app)
+      .post("/api/categories")
+      .send({ name: "valid_category" })
+
+    const { body } = await request(app)
+      .post("/api/items")
+      .send({
+        name: "valid_name",
+        model: "valid_model",
+        serialNumber: "valid_serial_number",
+        categoryId: category.body.id,
+        amount: 2,
+      })
+      .expect(201)
+
+    expect(body).toBeTruthy()
+    expect(body.id).toBeTruthy()
+    expect(body.name).toEqual("valid_name")
+    expect(body.model).toEqual("valid_model")
+    expect(body.serialNumber).toEqual("valid_serial_number")
+    expect(body.categoryId).toEqual(category.body.id)
+    expect(body.amount).toEqual(2)
+  })
+})
